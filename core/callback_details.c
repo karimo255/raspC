9c0d0e986d34f7ade7e72e95475bec70c2728998
karimo255 <karimo_255@hotmail.com>
2016-02-25 20:31:32 +0000

    adding protocol info

--- core/callback_details.c
+++ core/callback_details.c
@@ -14,8 +14,10 @@ struct net_live net_live;
 struct cpu_info cpu_i;
 struct storage_info storage_l;
 
+int zeuge=0;
 
 
+cJSON *root,*root_object;
 
 
 int callback_details(struct lws *wsi, enum lws_callback_reasons reason, void *user,
@@ -39,21 +41,23 @@ int callback_details(struct lws *wsi, enum lws_callback_reasons reason, void *us
 		}
 		case LWS_CALLBACK_ESTABLISHED:
 		{
-
+			pss->checked=0;
 			process("*****user info down******");
 			process(pss->session_id);
 			process(pss->user);
 
 			chalter=0;
 			increment_client_count();
-			//lws_callback_on_writable_all_protocol(lws_get_context(wsi),lws_get_protocol(wsi));
+			lws_callback_on_writable_all_protocol(lws_get_context(wsi),lws_get_protocol(wsi));
 
+			root = cJSON_CreateArray();
+			root_object=cJSON_CreateObject();
+			cJSON_AddItemToArray(root,root_object);
 
-			storageInfo(&storage_l);
-			cpuInfo(&cpu_i);
+			cJSON_AddStringToObject(root_object, "request", "count_client");            
+			cJSON_AddNumberToObject(root_object, "data", get_client_count());
 
-			char *out = hardwareStaticJSON(storage_l,cpu_i);
-
+			char *out=cJSON_Print(root);
 
 			int count = strlen(out);
 
@@ -62,29 +66,53 @@ int callback_details(struct lws *wsi, enum lws_callback_reasons reason, void *us
 
 			int n = sprintf((char *)p, "%s", out);
 
+			lws_write(wsi, p,  n, LWS_WRITE_TEXT); 
 
-			lws_write(wsi, p,  n, LWS_WRITE_TEXT);
-			free(out);           
-
-
-			break;
 		}
 		case LWS_CALLBACK_SERVER_WRITEABLE:
 		{         
+			if(zeuge!=get_client_count()){
+				pss->checked=0;
+				zeuge=get_client_count();
+			}
+			if(pss->checked==0){
+				storageInfo(&storage_l);
+				cpuInfo(&cpu_i);
 
-			char *out = hardwareDynamicJSON(cpu_l,ram_l,cpu_freq,net_live);
+				char *out = hardwareStaticJSON(storage_l,cpu_i);
 
 
-			int count = strlen(out);
+				int count = strlen(out);
 
-			unsigned char buffer[LWS_PRE + count];
-			unsigned char *p = &buffer[LWS_PRE];
+				unsigned char buffer[LWS_PRE + count];
+				unsigned char *p = &buffer[LWS_PRE];
 
-			int n = sprintf((char *)p, "%s", out);
+				int n = sprintf((char *)p, "%s", out);
 
 
-			lws_write(wsi, p,  n, LWS_WRITE_TEXT);
-			free(out);  
+				lws_write(wsi, p,  n, LWS_WRITE_TEXT);
+				free(out);           
+				process("erst check");
+				
+				pss->checked=1;
+
+				break;				
+
+			}else{
+				char *out = hardwareDynamicJSON(cpu_l,ram_l,cpu_freq,net_live);
+
+
+				int count = strlen(out);
+
+				unsigned char buffer[LWS_PRE + count];
+				unsigned char *p = &buffer[LWS_PRE];
+
+				int n = sprintf((char *)p, "%s", out);
+
+
+				lws_write(wsi, p,  n, LWS_WRITE_TEXT);
+				free(out);
+			}  
 			break;
 		}
 		case LWS_CALLBACK_RECEIVE:
