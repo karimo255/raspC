90bfbad10b07b2a4ca2702174050b2b0780707e5
5ouya <k.echchennouf@gmail.com>
2016-03-22 09:01:31 +0000

    cleaning project

--- go
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sudo make
-sudo make install
-sudo service raspC restart
--- packge_checker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for P; do
-    dpkg -s "$P" >/dev/null 2>&1 && {
-        echo "$P is installed."
-    } || {
-        echo "$P is not installed."
-    }
-done
